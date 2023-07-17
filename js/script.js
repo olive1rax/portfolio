f086f7e6fcb260dd274d2521af8c0bd31e904ace
olive1rax <contato@olive1ra.xyz>
2023-07-17 14:33:11 -0300

    added menu

--- js/script.js
+++ js/script.js
@@ -224,3 +224,12 @@ function onMouseLeave() {
   pointerY = null;
 
 }
+
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '9px';
+  }
+}
